2bbd2b3a796c917702b068a1ea2df0d15f05c6a4
Aram Services <aramservices198@hotmail.com>
2020-11-26 20:16:32 -0500

    Update

--- Madlibs/Python Files/py_free_code_camp.py
+++
@@ -1,7 +0,0 @@
-
-color = input("Enter a Color:")
-plural_noun = input("Enter a Plural Noun:")
-celebrity = input("Enter a Celebrity:")
-print("Roses are " + color + ".")
-print(plural_noun + " are blue")
-print("I love " + celebrity + ".")
