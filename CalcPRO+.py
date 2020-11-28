fbaa17a826f1040f4b550727ffa5f0768f2f79d2
Aram Services <aramservices198@hotmail.com>
2020-11-28 15:09:04 -0500

    Added an awesome calculator

---
+++ CalcPRO+.py
@@ -0,0 +1,34 @@
+print("Welcome to the PRO version of Calculator+! This calculator\nhas the ability to add, subtract, multiply,\ndivide. Remember that multiple numbers are not compatible with division. This calculator is only compatible with four numbers.")
+num1 = float(input("First Number?"))
+op = input("Enter operator, ex. +, -, *, /:")
+num2 = float(input("Second Number?"))
+num3 = float(0)
+num4 = float(0)
+neednum1 = ()
+neednum = input("Do you need another number? yes or no for our server is case sensitive. :)")
+if neednum == "yes":
+    num3 = float(input("Third Number?"))
+    neednum1 = input("Do you need another number? yes or no for our server is case sensitive. :)")
+elif neednum == "no":
+    print()
+
+if neednum1 == "yes":
+    num4 = float(input("Fourth Number?"))
+elif neednum == "no":
+    print()
+
+
+
+if op == "+":
+    print(num2 + num1 + num3 + num4)
+elif op == "-":
+    print(num1 - num2 + num3 - num4)
+elif op == "*":
+    print(num2 * num1 * num4 * num3)
+elif op == "/":
+    print(num1 / num2)
+else:
+    print("You have entered an incorrect character.")
+    print("To restart, you will need to re-run your calculator.")
+    exit()
+
