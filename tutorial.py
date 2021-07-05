d096e60c1681c4a589e73f2edec2f6e123e3dd63
Abdur-Rahman <aramservices198@gmail.com>
2021-07-05 12:19:12 -0400

    Update tutorial.py

--- tutorial.py
+++ tutorial.py
@@ -1,5 +1,5 @@
 welcome = input("\nWelcome, please enter your name\n")
 print("\nYour name is " + welcome + ".")
 ask = int(input("\nPlease enter a number\n"))
-num2 = int(input("\nPlease enter anothet number\n"))
+num2 = int(input("\nPlease enter another number\n"))
 print("The sum is ", ask + num2,".")
