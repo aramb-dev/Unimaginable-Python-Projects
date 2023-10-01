f10880a0e300bf90bcbd77262763883681158ed3
Abdur Rahman Bilal <aramb@aramservices.com>
2023-10-01 09:38:18 -0400

    Training on the turtle program
    I may migrate all of the training programs to another GitHub repo…

---
+++ Turtle (Square and Triangle)/square.py
@@ -0,0 +1,12 @@
+import turtle as t
+# Square using turle
+t.getscreen().bgcolor("white")
+t.title("Square using Turtle")
+t.color("red")
+t.forward(200)
+t.right(90)
+t.forward(200)
+t.right(90)
+t.forward(200)
+t.right(90)
+t.forward(200)
---
+++ Turtle (Square and Triangle)/triangle.py
@@ -0,0 +1,9 @@
+import turtle as t
+
+# Triangle using Turtle
+t.color("blue")
+t.forward(200)
+t.right(90)
+t.forward(200)
+t.right(90)
+t.home()
